33f3e0577989bead67bdee41f5d6255d92cfa7fe
senthil338 <48490092+senthil338@users.noreply.github.com>
2019-03-29 16:59:04 -0400

    Added file from TR system
    Added file from TR system

---
+++ CodingPractice/CodingPractice/StockBuySell.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Problems
+{
+    class Interval {
+       public int buy;
+       public int sell;
+    }
+    public class StockBuySell
+    {
+        /// <summary>
+        /// https://www.geeksforgeeks.org/stock-buy-sell/
+        /// </summary>
+        /// <param name="price"></param>
+        /// <param name="n"></param>
+     public static void stockBuySell(int[] price, int n) 
+     {
+         if (n == 1) { return; }
+         int count = 0;
+         List<Interval> sol = new List<Interval>();
+         int i = 0;
+         while (i < n - 1)
+         {
+             // Find Local Minima. Note that the limit is (n-2) as we are
+             // comparing present element to the next element. 
+             while ((i < n - 1) && (price[i + 1] <= price[i]))
+             {
+                 i++;
+             }
+             // If we reached the end, break as no further solution possible
+             if (i == n - 1)
+                 break;
+             Interval e = new Interval();
+             e.buy = i++;
+
+             // Find Local Maxima.  Note that the limit is (n-1) as we are
+             // comparing to previous element
+             while ((i < n) && (price[i] >= price[i - 1]))
+                 i++;
+
+             // Store the index of maxima
+             e.sell = i - 1;
+             sol.Add(e);
+
+             // Increment number of buy/sell
+             count++;
+         }
+
+          // print solution
+        if (count == 0)
+            System.Console.WriteLine("There is no day when buying the stock "
+                                                  + "will make profit");
+        else
+            for (int j = 0; j < sol.Count; j++)
+                System.Console.WriteLine("Buy on day: " + sol[j].buy
+                        + "        " + "Sell on day : " + sol[j].sell);
+         
+        return;
+     }
+
+     public static int maxProfit(int[] price, int n, int k)
+     {
+         // table to store results of subproblems
+         // profit[t][i] stores maximum profit using atmost
+         // t transactions up to day i (including day i)
+         int[,] profit = new int[k + 1, n + 1];
+
+         // For day 0, you can't earn money
+         // irrespective of how many times you trade
+         for (int i = 0; i <= k; i++)
+             profit[i, 0] = 0;
+
+         // profit is 0 if we don't do any transation
+         // (i.e. k =0)
+         for (int j = 0; j <= n; j++)
+             profit[0, j] = 0;
+
+         // fill the table in bottom-up fashion
+         for (int i = 1; i <= k; i++)
+         {
+             for (int j = 1; j < n; j++)
+             {
+                 int max_so_far = 0;
+
+                 for (int m = 0; m < j; m++)
+                     max_so_far = Math.Max(max_so_far, price[j] -
+                                    price[m] + profit[i - 1, m]);
+
+                 profit[i, j] = Math.Max(profit[i, j - 1], max_so_far);
+             }
+         }
+
+         return profit[k, n - 1];
+     }
+    }
+}
---
+++ CodingPractice/CodingPractice/StringManipulation_1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Books
+{
+    public static class StringManipulations
+    {
+        public static void StringPermutation(string str)
+        {
+            permutation(str, "");
+
+        }
+
+        private static void permutation(string str,string pre)
+        {
+
+            if(str.Length == 0)
+            {
+                Console.WriteLine(pre);
+            }
+            else
+            {
+                for(int i = 0; i < str.Length; i++)
+                {
+                    /*--------------------------------------------------------------
+                     |  i       |   str    |  rem  | Sub(0, i) | Sub(i + 1)| pre+str[i]
+                     |------    |-------------------------------------------------------
+                     |  0       |  ABC     |   BC  |    ""     |   BC      |   A    
+                     |  0       |   BC     |    C  |    ""     |    C      |   B
+                     |  0       |    C     |    "" |    ""     |   ""      |   C
+                     |          |          |       |           |           |
+                     |  1-BC    |    BC    |    B  |     B     |    ""     |    A + C
+                     |  0       |          |    "" |     ""    |    ""     |    A + C + B
+                     | 1-ABC    |    ABC   |  AC   |     A     |    C      |    B  
+                     |  0       |          |   C   |     ""    |    C      |    B + A
+                     |  0       |     C    |    "" |    ""     |     ""    |    B + A + C
+                     | 1-AC     |     AC   |    A  |     A     |    ""     |    B + C
+                     | 0-A      |     A    |    "" |    ""     |     ""    |    B + C + A
+                     | 2-ABC    |          |    AB |    OO     |     OO    |
+                     | 2-      |          |    OO |    OO     |     OO    |    OO
+                     | 000      |          |    OO |    OO     |     OO    |    OO
+                     | 000      |          |    OO |    OO     |     OO    |    OO
+                     | 00000    |          |    OO |    OO     |     OO    |    OO
+                     | 00000    |          |    OO |    OO     |     OO    |    OO
+                     | 00000    |          |    OO |    OO     |     OO    |    OO
+                  */
+                    string rem = str.Substring(0, i) + str.Substring(i + 1);
+                    permutation(rem, pre + str[i]);
+                }
+            }
+        }
+
+        public static void permute(string str, int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+
+        private static string swap(string a,int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
