695f3b4cd68d3df85a19593405adbd488c69b52e
senthil338 <48490092+senthil338@users.noreply.github.com>
2019-06-07 16:40:25 -0400

    Update FactoryDesign.cs

--- CodingPractice/CodingPractice/DesignPattern/FactoryDesign.cs
+++ CodingPractice/CodingPractice/DesignPattern/FactoryDesign.cs
@@ -1,66 +1,111 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace CodingPractice.DesignPattern
+namespace ConsoleApplication1.DesignPattern
 {
-    class FactoryDesign
+    public class FactoryMethod
     {
-        public void main()
-        {
-            IShap circle = ShapeFactory.GetShap("Circle");
-            IShap rectangle = ShapeFactory.GetShap("Rectangle");
-        }
-    }
 
-    public class ShapeFactory
-    {
-        public static IShap GetShap(string shape)
+        /*
+         1. Product - This is an interface for creating the objects.
+         2. ConcreteProduct - This is a class which implements the Product interface.
+         3. Creator - This is an abstract class and declares the factory method, which returns an object of type Product.
+         4. ConcreteCreator - This is a class which implements the Creator class and overrides the factory method to return an instance of a ConcreteProduct.
+         */
+
+        #region 1. Product interface or abstract class
+        /// <summary>
+        /// 1. Product
+        /// </summary>
+        public interface IDrive
         {
-            if(shape== "Square")
+            void Drive();
+        }
+
+        public class Air : IDrive
+        {
+            public void Drive()
             {
-                return new Square();
+               // throw new NotImplementedException();
             }
-            else if(shape == "Rectangle")
+        }
+        #endregion
+
+        #region 2. ConcreteProduct like Car, Bus etc which implement common product behavior 
+        /// <summary>
+        /// 2. ConcreteProduct
+        /// </summary>
+        public class Bus : IDrive
+        {
+            public void Drive()
             {
-                return new Rectangle();
+                Console.WriteLine("Bus can drive {0} km", 20);
             }
-            else if (shape == "Circle")
+        }
+
+        public class Car : IDrive
+        {
+            public void Drive()
             {
-                return new Rectangle();
+                Console.WriteLine("Car can drive {0} km", 50);
             }
-            return null;
         }
-    }
 
-     public interface IShap
-    {
-        void Draw();
-    }
-
-    public class Square : IShap
-    {
-        public void Draw()
+        public class Scooter : IDrive
         {
-            Console.WriteLine("Square");
+            public void Drive()
+            {
+                Console.WriteLine("Scooter can drive {0} km", 100);
+            }
         }
-    }
+        #endregion
 
-    public class Rectangle : IShap
-    {
-        public void Draw()
+        #region 3. Creator -> interface or abstract class for concreate class interface whihc are implemented by Product 
+        public interface IVehicleFactory
         {
-            Console.WriteLine("Rectangle");
+            IDrive GetVehicleKm(VehicleType vehicleType);
         }
-    }
-
-    public class Circle : IShap
-    {
-        public void Draw()
+        //consider this will be supplied dynamically based on clients
+        public enum VehicleType
         {
-            Console.WriteLine("Circle");
+            Car,
+            Bus,
+            Scooter,
+            Air
+        }
+
+        #endregion
+
+        #region 4. ConcreteCreator , implement creator
+        public class ConcreteVehicleFactory : IVehicleFactory
+        {
+            public IDrive GetVehicleKm(VehicleType vehicleType)
+            {
+                switch (vehicleType)
+                {
+                    case VehicleType.Scooter:
+                        return new Scooter();
+                    case VehicleType.Bus:
+                        return new Bus();
+                    case VehicleType.Car:
+                        return new Car();
+                    default:
+                        throw new ApplicationException(string.Format("Vehicle cannot be created"));
+                }
+
+            }
+        }
+        #endregion
+
+       //Client Code
+        static void Main_11(string[] args)
+        {
+            ConcreteVehicleFactory factoryMethod = new ConcreteVehicleFactory();
+            IDrive vehicle = factoryMethod.GetVehicleKm(VehicleType.Car);
+            vehicle.Drive();
         }
     }
-
-
 }
