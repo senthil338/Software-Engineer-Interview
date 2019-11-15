cf272e116a1e67be458f4df91eef9315b97daa91
Senthil <786.senthil@gmail.com>
2019-11-15 09:59:24 -0500

    new

---
+++ CodingPractice/CodingPractice/SortingAlgorithm/HeapSort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingPractice.SortingAlgorithm
+{
+    class HeapSort
+    {
+        public void sort(int[] arr)
+        {
+            int len = arr.Length;
+
+            // Build heap (rearrange array) 
+            for (int i = len / 2 - 1; i >= 0; i--)
+                heapify(arr, len, i);
+
+            // One by one extract an element from heap 
+            for (int i = len - 1; i >= 0; i--)
+            {
+                // Move current root to end 
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                // call max heapify on the reduced heap 
+                heapify(arr, i, 0);
+            }
+        }
+
+        // To heapify a subtree rooted with node i which is 
+        // an index in arr[]. n is size of heap 
+        void heapify(int[] arr, int n, int i)
+        {
+            int largest = i; // Initialize largest as root 
+            int l = 2 * i + 1; // left = 2*i + 1 
+            int r = 2 * i + 2; // right = 2*i + 2 
+
+            // If left child is larger than root 
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            // If right child is larger than largest so far 
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            // If largest is not root 
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                // Recursively heapify the affected sub-tree 
+                heapify(arr, n, largest);
+            }
+        }
+
+        /* A utility function to print array of size n */
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.Read();
+        }
+    }
+}
