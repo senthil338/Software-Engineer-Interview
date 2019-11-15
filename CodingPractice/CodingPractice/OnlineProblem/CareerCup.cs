f6b904cf7ade0beaa30f996408a02d61e346e436
Senthil <786.senthil@gmail.com>
2019-11-15 09:55:59 -0500

    new

---
+++ CodingPractice/CodingPractice/OnlineProblem/CareerCup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingPractice.OnlineProblem
+{
+   public class CareerCup
+    {
+        public static Dictionary<int, int> GenerateSet()
+        {
+            // https://www.careercup.com/question?id=5095657344860160
+            //int[,] set = { { 1, 2 }, { 1, 3 }, { 2, 4 } }; // 1,3 & 2, 4
+           // int[,] set = { { 1, 2 }, { 1, 3 }, { 1, 1 }, { 2, 4 },{ 1, 5 },{ 5, 1 },{ 5, 4 } };
+            //1,5 & 2,4 & 5,4
+            int[,] set = { { 1, 2 }, { 1, 3 }, { 1, 1 }, { 2, 4 },{ 1, 5 },{ 4, 9 },{ 5, 4 },{ 2, 8 } };
+            //1,5 & 2,8 & 5,4 & 4,9
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for(int i = 0; i < set.GetLength(0); i++)
+            {
+                if (!map.ContainsKey(set[i, 0]) && !map.ContainsKey(set[i, 1]) && set[i, 0] != set[i, 1])
+                {
+                    map.Add(set[i, 0], set[i, 1]);
+                    map.Add(set[i, 1], set[i, 0]);
+                }
+                else if (map.ContainsKey(set[i, 0]) && map[set[i, 0]] < set[i, 1])
+                {
+                    map[set[i, 0]] = set[i, 1];
+                }
+                if (!map.ContainsKey(set[i, 0]))
+                {
+                    map.Add(set[i, 0], set[i, 1]);
+                }
+                if (!map.ContainsKey(set[i, 1]))
+                {
+                    map.Add(set[i, 1], 0);
+                }
+                
+            }
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            foreach (KeyValuePair<int, int> key in map)
+            {
+                if (key.Value != 0)
+                {
+                    result.Add(key.Key, key.Value);
+                }
+            }
+            return result;
+        }
+
+        public static List<int> getIntersection(int[] arr1, int[] arr2)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        list.Add(arr1[i]);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ CodingPractice/CodingPractice/OnlineProblem/Glassdoor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingPractice.OnlineProblem
+{
+    public class Glassdoor
+    {
+
+        public static void MergeSort()
+        {
+            int[] arr = { 4, 6, 7, 2, 4, 10, 56 };
+
+        }
+
+        public static void MergeSort(int[] arr, int leftStart,int rightEnd,int[] temp)
+        {
+            if(leftStart >= rightEnd)
+            {
+                return;
+            }
+            int Middle =  (leftStart + rightEnd) / 2;
+            MergeSort(arr, leftStart, Middle, temp);
+            MergeSort(arr, Middle + 1, rightEnd, temp);
+
+        }
+
+        public static void Merge(int[] arr, int leftStart, int rightEnd, int[] temp)
+        {
+            int leftEnd = (leftStart + rightEnd) / 2;
+            int rightStart = leftEnd + 1;
+            int left = leftStart;
+            int right = rightStart;
+            int index = leftStart;
+            while(left <= leftEnd && right <= rightEnd)
+            {
+                if(arr[left] <= arr[right])
+                {
+                    temp[index] = arr[left];
+                    left++;
+                }
+                else{
+                    temp[index] = arr[right];
+                    right++;
+                }
+                index++;
+            }
+            while(left <= leftEnd)
+            {
+                temp[index] = arr[left];
+                left++;
+            }
+
+            while (right <= rightEnd)
+            {
+                temp[index] = arr[right];
+                left++;
+            }
+            
+        }
+        /// <summary>
+        /// Asked in FB, Microsofts
+        /// ou are given a list containing k sorted lists of integers , each list is of varying size, 
+        /// max of which is n. Implement an iterator in java that with each call to its next() 
+        /// function retrieves the next integer in the overall order of all the integers in all the 
+        /// lists combined. Answer should be efficient depending on the number of calls to next(). 
+        /// Example: lists = [[1,2,3],[1,4],[2,5,7,8]] n = 4 k = 3 
+        /// iterator should return (if called 9 times): 1,1,2,2,3,4,5,7,8
+        /// https://careercup.com/question?id=5647438614888448
+        /// https://www.glassdoor.com/Interview/You-are-given-a-list-containing-k-sorted-lists-of-integers-each-list-is-of-varying-size-max-of-which-is-n-Implement-an-QTN_913587.htm
+        /// </summary>
+        public static void SortKSortedList()
+        {
+            int[] a = { 1, 2, 3 };
+            int[] b = { 2, 4 };
+            int[] result1 = mergeArray(a, b);
+            int[] c = { 2, 5, 7, 8 };
+            int[] result2 = mergeArray(result1, c);
+            
+        }
+
+        public static int[] mergeArray(int[] a, int[] b)
+        {
+            int[] answer = new int[a.Length + b.Length];
+            int i = 0, j = 0, k = 0;
+
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                    answer[k++] = a[i++];
+
+                else
+                    answer[k++] = b[j++];
+            }
+
+            while (i < a.Length)
+                answer[k++] = a[i++];
+
+
+            while (j < b.Length)
+                answer[k++] = b[j++];
+
+            return answer;
+        }
+    }
+}
