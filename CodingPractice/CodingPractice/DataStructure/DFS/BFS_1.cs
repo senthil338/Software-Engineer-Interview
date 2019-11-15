2846951d1918af705f08ac047b05338d994c5c63
Senthil <786.senthil@gmail.com>
2019-11-15 09:58:37 -0500

    new

---
+++ CodingPractice/CodingPractice/DataStructure/DFS/BFS_1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingPractice.DataStructure.DFS
+{
+    class BFS_1
+    {
+        static void Main_2(string[] args)
+        {
+            Node topNode = new Node() { Value = 1 };
+            topNode.Neighbours.Add(new Node()
+            {
+                Value = 2,
+                Neighbours = new List<Node> { new Node() { Value = 5,
+                    Neighbours = new List< Node > { new Node() { Value = 9 } } }
+                }
+            });
+            topNode.Neighbours.Add(new Node()
+            {
+                Value = 3,
+                Neighbours = new List<Node> { new Node() { Value = 6,
+                    Neighbours = new List<Node>() { new Node() { Value = 10 } } }, new Node() { Value = 7 } }
+            });
+            topNode.Neighbours.Add(new Node() { Value = 4,
+                Neighbours = new List<Node>() { new Node() { Value = 8 } } });
+
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(topNode);
+
+            while (queue.Count > 0)
+            {
+                var element = queue.Dequeue();
+                if (element.Neighbours.Count > 0)
+                {
+                    Console.WriteLine(element.Value);
+                    foreach (var item in element.Neighbours)
+                    {
+                        queue.Enqueue(item);
+                    }
+                }
+                else
+                    Console.WriteLine(element.Value);
+            }
+
+            Console.Read();
+        }
+
+        public class Node
+        {
+            public Node()
+            {
+                Neighbours = new List<Node>();
+            }
+
+            public int Value { get; set; }
+            public IList<Node> Neighbours { get; set; }
+        }
+    }
+}
